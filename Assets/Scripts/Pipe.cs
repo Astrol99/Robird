ef75f35f462144159d34a0888ac79a033c18925b
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2021-01-02 17:13:40 -0600

    Inital commit

---
+++ Assets/Scripts/Pipe.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    public float speed = 5f;
+    public float selfDestroyDelay = 10f;
+    private bool updatedScore = false;
+
+    private void Start()
+    {
+        Destroy(gameObject, selfDestroyDelay);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * speed);
+
+        if (transform.position.x < 0 && !updatedScore)
+        {
+            FindObjectOfType<Score>().score += 1;
+            FindObjectOfType<AudioManager>().Play("PlayerScore");
+            updatedScore = true;
+        }
+    }
+}
